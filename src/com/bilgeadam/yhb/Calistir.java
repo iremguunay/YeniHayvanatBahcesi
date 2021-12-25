c021eaf18af3e69308c164154ef317ed5237429c
Irem Gunay <iremguunay@gmail.com>
2021-12-25 13:51:00 +0300

    Initial commit

---
+++ src/com/bilgeadam/yhb/Calistir.java
@@ -0,0 +1,59 @@
+package com.bilgeadam.yhb;
+
+public class Calistir {
+
+    /*public void seslendir(Kopek kopek) {
+        kopek.sesCikar();
+    }
+
+    public void seslendir(Kedi kedi) {
+        kedi.sesCikar();
+    }*/
+
+    public void seslendir(Hayvan hayvan) {
+        hayvan.sesCikar();
+    }
+
+
+    public static void main(String[] args) {
+        Calistir calistir = new Calistir();
+
+        //Kopek kopek1 = new Kopek();
+
+        //Polymorphism
+        //Alt class objesini, üst class değişkeniyle oluşturma
+
+        Hayvan kopek1 = new Kopek();
+        kopek1.setNo(1);
+        kopek1.setAd("Garip");
+        kopek1.setAgirlik(30);
+        kopek1.setUzunluk(1.5);
+
+        System.out.println(kopek1);
+        calistir.seslendir(kopek1);
+
+        System.out.println("--------------");
+
+        //Kedi kedi1 = new Kedi();
+        Hayvan kedi1 = new Kedi();
+        kedi1.setNo(2);
+        kedi1.setAd("Sarman");
+        kedi1.setAgirlik(5);
+        kedi1.setUzunluk(0.4);
+
+        System.out.println(kedi1);
+        calistir.seslendir(kedi1);
+
+        System.out.println("--------------");
+
+        Hayvan esek1 = new Esek();
+        esek1.setNo(3);
+        esek1.setAd("Karakaçan");
+        esek1.setAgirlik(110);
+        esek1.setUzunluk(2.3);
+
+        System.out.println(esek1);
+        calistir.seslendir(esek1);
+    }
+
+}
---
+++ src/com/bilgeadam/yhb/Hayvan.java
@@ -0,0 +1,58 @@
+package com.bilgeadam.yhb;
+
+public class Hayvan {
+
+    private int no;
+    private String ad;
+    private double agirlik;
+    private double uzunluk;
+
+    public Hayvan() {
+    }
+
+    @Override
+    public String toString() {
+        return "Hayvan{" +
+                "no=" + no +
+                ", ad='" + ad + '\'' +
+                ", agirlik=" + agirlik +
+                ", uzunluk=" + uzunluk +
+                '}';
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public double getAgirlik() {
+        return agirlik;
+    }
+
+    public void setAgirlik(double agirlik) {
+        this.agirlik = agirlik;
+    }
+
+    public double getUzunluk() {
+        return uzunluk;
+    }
+
+    public void setUzunluk(double uzunluk) {
+        this.uzunluk = uzunluk;
+    }
+
+    public void sesCikar() {
+        System.out.println("------------");
+    }
+}
